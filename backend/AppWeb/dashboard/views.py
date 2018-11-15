a6be7d4ad44fba4bd90fa27f97b526a9b89bbee9
bichocj <bichocj@gmail.com>
2018-11-15 16:45:29 -0500

    added webhook

--- backend/AppWeb/dashboard/views.py
+++ backend/AppWeb/dashboard/views.py
@@ -15,12 +15,15 @@ def dashboard(request):
 
 @csrf_exempt
 def webhook(request):
+    print(request.method)
     print(request.GET)
     print(request.POST)
+    print(request.body)
+
     challenge = request.GET.get("hub.challenge", "")
     token = request.GET.get("hub.verify_token", "")
     mode = request.GET.get('hub.mode', "")
     if token == 'abc1234':
         return HttpResponse(challenge)
 
-    return HttpResponse("%s %s" % (request.method, request.body))
+    return HttpResponse('you are an error!')
