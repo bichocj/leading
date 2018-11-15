42343e58863a641dd99f32391f6abcbadf58f0b9
bichocj <bichocj@gmail.com>
2018-11-15 16:30:00 -0500

    added webhook

--- backend/AppWeb/dashboard/views.py
+++ backend/AppWeb/dashboard/views.py
@@ -14,10 +14,11 @@ def dashboard(request):
 
 def webhook(request):
     print(request.GET)
+    print(request.POST)
     challenge = request.GET.get("hub.challenge", "")
     token = request.GET.get("hub.verify_token", "")
     mode = request.GET.get('hub.mode', "")
-    if mode == 'subscribe' and token == 'abc1234':
+    if token == 'abc1234':
         return HttpResponse(challenge)
 
     return HttpResponse('you are an error!')
